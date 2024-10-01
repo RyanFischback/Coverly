89f0a38395de697ee3c27ee4fd0afa86b62ac3fa
rfischback <ryan-fischback@hotmail.com>
2024-10-01 12:48:08 -0300

    about page styles

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,15 +2,18 @@
 <template>
   <div class="about-page">
     <section class="about">
-      <div class="container">
+      <div>
         <h2>What is Coverly?</h2>
-        <p>
+        <p class="text">
           Coverly is a user-friendly web application designed to streamline the
           process of generating professional cover letters through user-provided
           prompts. This project aims to offer a seamless and efficient
           experience, enabling users to create personalized and impactful cover
           letters with minimal effort.
         </p>
+        <router-link to="/demo" class="get-started-button"
+          >Get Started</router-link
+        >
       </div>
     </section>
   </div>
@@ -20,4 +23,8 @@
 
 <style>
 /* Add styles here */
+.text {
+  max-width: 500px;
+  text-align: center;
+}
 </style>
